cf8107b317acf99bf8971ed8a8d3c3c23608ebc7
Melanie <mejr5b@gmail.com>
2020-01-07 14:10:09 -0600

    studio for chapter 12

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
-    @RequestMapping("user")
-    public String
+
+    @GetMapping("/add")
+    public String displayAddUserForm(){
+        return "user/add";
+    }
 }
